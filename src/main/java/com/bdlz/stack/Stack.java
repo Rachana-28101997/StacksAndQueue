616e951035c336a27637816babe0d04ba37949f9
Rachana <rachanarrachu28@gmail.com>
2020-11-04 21:52:13 +0530

    Added : Stack class added

---
+++ src/main/java/com/bdlz/stack/Stack.java
@@ -0,0 +1,17 @@
+package com.bdlz.stack;
+
+public class Stack<K> extends MyLinkedList<K>{
+	private final MyLinkedList myLinkedList;
+
+	public Stack() {
+		this.myLinkedList = new MyLinkedList();
+	}
+	
+	public void push(INode myNode) {
+		myLinkedList.add(myNode);
+	}
+	
+	public INode peak() {
+		return myLinkedList.head;
+	}
+}
